f91054ec4370669c30cf5e646a33ca25edc4925f
Abinaya353 <abinaya1153@gmail.com>
2026-01-29 16:32:43 +0530

    A completed the work

--- src/Sample.java
+++ src/Sample.java
@@ -2,11 +2,22 @@
 public class Sample {
 	public static void main(String[] args) {
 		
+		int sum = 10;
+		System.out.println(sum);
 		
+		int Equal = 10;
+		System.out.println(Equal);
+
+		int divide = 20;
+		System.out.println(divide);
 		
+		int num = 100;
+		System.out.println(num);
+
 		
+
 		
 		
 	}
-
+ 
 }
